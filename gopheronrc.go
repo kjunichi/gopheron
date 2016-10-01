5e11474fc2ef46a8ec79839b29bdfa7e29c1d663
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-10-02 07:26:55 +0900

    Use golang

---
+++ gopheronrc.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+		"fmt"
+		"os"
+    "github.com/zhouhui8915/go-socket.io-client"
+    "log"
+)
+
+func main() {
+		fmt.Println("os.Args: ", os.Args)
+		fmt.Println("msg = ", os.Args[1])
+    opts := &socketio_client.Options{
+        Transport: "websocket",
+        Query:     make(map[string]string),
+    }
+    
+		uri := "http://localhost:5050/socket.io/"
+
+    client, err := socketio_client.NewClient(uri, opts)
+    if err != nil {
+        log.Printf("NewClient error:%v\n", err)
+        return
+    }
+
+    client.On("error", func() {
+        log.Printf("on error\n")
+    })
+    client.On("connection", func() {
+        log.Printf("on connect\n")
+    })
+    client.On("disconnection", func() {
+        log.Printf("on disconnect\n")
+    })
+
+    //client.Emit("gopher send", "そうとくんなおくんなかよくあそぼう！")
+		client.Emit("gopher send", os.Args[1])
+    
+}

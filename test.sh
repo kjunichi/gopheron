b09148a20613c46fb31725807f4634ccd314b8a5
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-11-17 18:07:35 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,10 @@ electron . &
 sleep 3
 import -window root gopheron.png
 composite -compose difference no.png gopheron.png diff.png
-result=`identify -format "%[mean]" diff.png`
-if [$result -gt 0]
-then
+result=`identify -format "%[mean]" diff.png|awk '{printf("%d",$1 + 0.5)}'`
+if [ $result -gt 0 ] ; then
  echo "OK"
 else
- exit -1
+ exit 1
 fi
 

14fa3a61dc9471ca9771af16c7159db5517e0524
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-11-17 17:34:25 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 import -window root >no.png
 electron . &
 sleep 3
-import -window root >gopheron.png
+import -window root gopheron.png
 composite -compose difference no.png gopheron.png diff.png
 identify -format "%[mean]" diff.png

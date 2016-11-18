829fe9345793869c10c9e778832b0a0e85f1ee7a
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-11-18 09:30:39 +0900

    Update build_osmesa.sh

--- build_osmesa.sh
+++ build_osmesa.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-pushd .
+savedDir=`pwd`
 mkdir chromium
 cd chromium
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=PATH:/path/to/depot_tools
+export PATH=$PATH:$savedDir/chromium/depot_tools
 fetch --nohooks --no-history chromium
 ./build/install-build-deps.sh
 gclient runhooks
 cd src
 gn gen out/Default
 ninja -C out/Default osmesa
-popd
+cd $savedDir

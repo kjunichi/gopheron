d67841a02a51fe7421a911893842f9896f05bc9c
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-11-18 10:30:33 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 import -window root no.png
-electron . &
+electron --use-gl=osmesa . &
 sleep 3
 import -window root gopheron.png
 composite -compose difference no.png gopheron.png diff.png

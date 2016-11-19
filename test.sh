47c038fd962b4cd58712d6e3d20387a99c64861a
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-11-19 09:07:39 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-import -window root no.png
+import -window root no.jpg
 electron --use-gl=osmesa . &
 sleep 8
-import -window root gopheron.png
-composite -compose difference no.png gopheron.png diff.png
-result=`identify -format "%[mean]" diff.png|awk '{printf("%d",$1 + 0.5)}'`
+import -window root gopheron.jpg
+composite -compose difference no.jpg gopheron.jpg diff.jpg
+result=`identify -format "%[mean]" diff.jpg|awk '{printf("%d",$1 + 0.5)}'`
 if [ $result -gt 0 ] ; then
  echo "OK"
 else
@@ -16,4 +16,4 @@ cd imgcat;CC=clang make;cd ..
 pwd
 ls -ltr
 identify -list format
-imgcat/src/imgcat --width 80 ./gopheron.png
+imgcat/src/imgcat --width 80 ./gopheron.jpg
